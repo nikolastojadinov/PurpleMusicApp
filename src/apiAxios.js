209b4ba3e19244a97a465c955af663c6239224d2
nikolastojadinov <nikolastojadinov@yahoo.co.uk>
2025-09-26 20:04:39 +0000

    Set axios baseURL for production to backend URL

--- src/apiAxios.js
+++ src/apiAxios.js
@@ -2,7 +2,12 @@
 import { apiKey } from './config';
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://purplemusicapp.onrender.com' // Itt add meg a backend URL-t
+  : '';
+
 const apiAxios = axios.create({
+  baseURL,
   headers: {
     Authorization: `Bearer ${apiKey}`,
   },
